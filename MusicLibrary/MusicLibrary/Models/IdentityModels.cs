1b739fa310f8cbfee08580d1f8988fa7e6237c9a
itslerrie <valeria.kostadinova@gmail.com>
2016-05-31 13:50:11 +0300

    FixFirstError

--- MusicLibrary/MusicLibrary/Models/IdentityModels.cs
+++ MusicLibrary/MusicLibrary/Models/IdentityModels.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
 
 namespace MusicLibrary.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public virtual ICollection<Playlist> Playlist { get; set; }
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
